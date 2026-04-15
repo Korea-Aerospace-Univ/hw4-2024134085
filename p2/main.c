22ecc0e43d86abee35b86cbc398cd09d9aa9945a
2024134085 <james050527@gmail.com>
2026-04-15 15:48:44 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, cCnt=0, cMax=0, iCnt=0, iMax=0;
+    
+    scanf("%d", &n);
+    char str[i];
+    
+    for(i=0;i<n;i++){
+        scanf("%s", &str);
+        
+        if(str[i]>='a' && str[i]<='z'){
+            cCnt++;
+            iCnt = 0;
+            if(cCnt > cMax){
+                cMax = cCnt;
+            }
+        }
+        if(str[i]>='0' && str[i]<='9'){
+            iCnt++;
+            cCnt = 0;
+            if(iCnt > iMax){
+                iMax = iCnt;
+            }
+        }
+        else{
+            cCnt = 0;
+            iCnt = 0;
+        }
+    }
+    printf("%d",cMax);
+    printf("%d",iMax);
+    
+    return 0;
+}
